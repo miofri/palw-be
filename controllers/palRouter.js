327442d731f7f0f9d42401d32c52f29693ba14e6
Selvi <slvkesuma@gmail.com>
2024-03-18 11:38:16 +0200

    building post request for parents combo

--- controllers/palRouter.js
+++ controllers/palRouter.js
@@ -2,7 +2,8 @@ const express = require('express');
 const palRouter = require('express').Router();
 const path = require('path');
 const Pals = require('../models/palSchema');
-const palProjection = require('../models/palProjection');
+const PalCombos = require('../models/palCombos/palCombosSchema');
+const palProjection = require('../models/allPals/palProjection');
 
 const palImagePath = path.join(__dirname, '..', 'assets', 'palIcons');
 const palWorkIconsPath = path.join(__dirname, '..', 'assets', 'palWorkIcons');
@@ -12,6 +13,38 @@ palRouter.use('/palworkicons', express.static(palWorkIconsPath));
 palRouter.get('/', (req, res) => {
 	Pals.find({}, palProjection).then((pals) => {
 		res.json(pals);
+	});
+});
+
+palRouter.get('/palcombos', (req, res) => {
+	PalCombos.find({}).then((pals) => {
+		res.json(pals);
+	});
+});
+
+palRouter.post('/palcombos/', (req, res) => {
+	const requestedPal = req.body.palName;
+	const foundPal = [];
+	PalCombos.find({}).then((docs) => {
+		const foundPal = docs.filter((element) => {
+			for (const key in element) {
+				if (element[key] === requestedPal) {
+					return true;
+				}
+			}
+			return false;
+		});
+		const filteredPal = [];
+		foundPal.filter((doc) => {
+			for (const key in doc) {
+				if (doc[key] === requestedPal) {
+					filteredPal.push({ [key]: doc[key], Pal: doc.Pal });
+				}
+			}
+		});
+
+		console.log(filteredPal);
+		res.json(filteredPal);
 	});
 });
 
---
+++ models/allPals/palSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const palSchema = new mongoose.Schema({
+	Name: String,
+	CodeName: String,
+	ZukanIndex: Number,
+	Rarity: Number,
+	ElementType1: String,
+	ElementType2: String,
+	CombiRank: Number,
+	IndexOrder: Number
+});
+
+palSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+	},
+});
+
+const Pals = mongoose.model('pals', palSchema);
+module.exports = Pals;
